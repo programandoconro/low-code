96950bf4b5c97d2852bda9ac73dc97e48e3fc759
programandoconro <programandoconro@gmail.com>
2025-07-03 18:45:15 +0900

    drag and drop title element and local-store it

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,84 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <nav>
+      <div class="h1" id="h1" ref="h1Ele" draggable="true">h1</div>
+    </nav>
+    <div dropzone="copy" id="canvas" class="canvas">canvas</div>
   </main>
 </template>
 
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const h1 = document.getElementById("h1");
+  const canvas = document.getElementById("canvas");
+  const storedCanvas = localStorage.getItem("canvas");
+  if (storedCanvas) {
+    canvas.innerHTML = storedCanvas;
+  }
+
+  h1.addEventListener("dragstart", (e) => {
+    console.log("dragstart");
+    e.dataTransfer.effectAllowed = "all";
+    e.dataTransfer.setData("text/plain", "h1");
+  });
+
+  ["dragenter", "dragover"].forEach((eventName) => {
+    canvas.addEventListener(eventName, (e) => {
+      console.log(eventName);
+      e.preventDefault();
+    });
+  });
+
+  canvas.addEventListener("drop", function (e) {
+    console.log("dropped");
+
+    const draggedType = e.dataTransfer.getData("text/plain");
+
+    if (draggedType === "h1") {
+      const newH1 = document.createElement("h1");
+      newH1.innerText = "Title";
+      newH1.contentEditable = true;
+      this.appendChild(newH1);
+      storeCanvasInnerHtml();
+    }
+    e.preventDefault();
+  });
+
+  canvas.addEventListener("input", () => {
+    console.log("inputting");
+    storeCanvasInnerHtml();
+  });
+  function storeCanvasInnerHtml() {
+    localStorage.setItem("canvas", document.getElementById("canvas").innerHTML);
+  }
+});
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+main {
+  display: flex;
+  height: 100vh;
+  width: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+nav {
+  background-color: gray;
+  width: 30%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.h1 {
+  border: 1px solid whitesmoke;
+  text-align: center;
+  padding: 10px;
+  margin: 10px;
+  cursor: grab;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.canvas {
+  width: 70%;
+  background-color: black;
+  border: 2px dashed white;
 }
 </style>
